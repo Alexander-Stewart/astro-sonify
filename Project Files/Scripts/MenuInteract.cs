241936faf53fdec30b83cf9dfdd60731a7db1f5c
Alexander-Stewart <32624541+Alexander-Stewart@users.noreply.github.com>
2018-07-02 16:38:24 -0400

    7/02/18 Update

--- Project Files/Scripts/MenuInteract.cs
+++ Project Files/Scripts/MenuInteract.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿//#define SOUND
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
+using RealSpace3D;
 
 public class MenuInteract : VRTK_InteractableObject {
 
@@ -9,6 +11,9 @@ public class MenuInteract : VRTK_InteractableObject {
 
     private bool showText = false;
     private GameObject canvas;
+#if SOUND
+    private RealSpace3D_AudioSource audioSource;
+#endif
 
     public string description;
     public string title;
@@ -19,6 +24,9 @@ public class MenuInteract : VRTK_InteractableObject {
         showText = true;
         canvas.SetActive(true);
         Debug.Log("Shit is on");
+#if SOUND
+        audioSource.rs3d_PlaySound();
+#endif
     }
 
     public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
@@ -27,13 +35,19 @@ public class MenuInteract : VRTK_InteractableObject {
         showText = false;
         canvas.SetActive(false);
         Debug.Log("Shit has stopped");
+#if SOUND
+        audioSource.rs3d_StopSound();
+#endif
     }
 
 
     // Use this for initialization
     void Start () {
         canvas = GameObject.FindGameObjectWithTag("Canvas");
-	}
+#if SOUND
+        audioSource = gameObject.transform.GetChild(1).gameObject.GetComponent<RealSpace3D_AudioSource>();
+#endif
+    }
 	
 	// Update is called once per frame
 	protected override void Update () {
