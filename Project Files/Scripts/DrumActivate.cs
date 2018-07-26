c4eacd143fd4a80c00571573df3f4443c36d8364
Alexander-Stewart <32624541+Alexander-Stewart@users.noreply.github.com>
2018-07-26 16:28:20 -0400

    07/26/18 Update

---
+++ Project Files/Scripts/DrumActivate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class DrumActivate : MonoBehaviour {
+
+    private AudioSource drumSource;
+
+	// Use this for initialization
+	void Start () {
+
+        // getting the audio source
+        drumSource = GetComponent<AudioSource>();
+        
+	}
+	
+    // for starting the clip
+    void OnCollisionEnter(Collision col)
+    {
+        if (!drumSource.isPlaying)
+        {
+            drumSource.Play();
+        }
+    }
+
+    // for ending the clip
+    void OnCollisionExit(Collision col)
+    {
+        if(drumSource.isPlaying)
+        {
+            drumSource.Stop();
+        }
+    }
+}
