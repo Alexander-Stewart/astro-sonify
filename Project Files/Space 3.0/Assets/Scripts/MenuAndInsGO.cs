059c83decbae5a8e96553092a9fe93357ecb9f4d
Alexander-Stewart <32624541+Alexander-Stewart@users.noreply.github.com>
2018-11-06 20:20:28 -0500

    Go Update
    Updated scripts and scenes for compatability with the Oculus Go

---
+++ Project Files/Space 3.0/Assets/Scripts/MenuAndInsGO.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using UnityEngine.UI;
+
+public class MenuAndInsGO : MonoBehaviour {
+    private bool onOffSwitch = true;
+    private Text screenText;
+    private Text title;
+    private GameObject canvas;
+    private bool startUp = true;
+
+    protected VRTK_ControllerEvents contrlEvents;
+
+    // Use this for initialization
+    void Start()
+    {
+        contrlEvents = GetComponent<VRTK_ControllerEvents>();
+
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+
+        GameObject textObj1 = canvas.transform.GetChild(0).gameObject;
+        title = textObj1.GetComponent<Text>();
+
+        GameObject textObj2 = canvas.transform.GetChild(1).gameObject;
+        screenText = textObj2.GetComponent<Text>();
+
+       
+
+    }
+
+    private void MenuAndInstructions_ButtonTwoReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("Emitted");
+        onOffSwitch = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canvas == null)
+        {
+            canvas = GameObject.FindGameObjectWithTag("Canvas");
+
+            GameObject textObj1 = canvas.transform.GetChild(0).gameObject;
+            title = textObj1.GetComponent<Text>();
+
+            GameObject textObj2 = canvas.transform.GetChild(1).gameObject;
+            screenText = textObj2.GetComponent<Text>();
+        }
+        else if (contrlEvents.buttonTwoPressed)
+        {
+            // changing the onOffSwitch.
+            if (onOffSwitch)
+            {
+                onOffSwitch = false;
+            } else
+            {
+                onOffSwitch = true;
+            }
+
+            // changing the canvas.
+            if (onOffSwitch)
+            {
+                title.text = "Controls:";
+                screenText.text = "Pointer: Trigger Button" +
+                    "\n" +
+                    "Movement: point controller in wanted direction, and slide thumb forward on touchpad." +
+                    "\n" +
+                    "To close this window, press the back button.";
+                canvas.SetActive(true);
+            } else
+            {
+                canvas.SetActive(false);
+                onOffSwitch = false;
+            }
+            
+        }
+
+    }
+
+}
